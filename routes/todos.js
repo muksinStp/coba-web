9677dcb4d4c24b7a143a7b0162a165982f998d54
muksinStp <muksinstp787@gmail.com>
2024-12-27 17:59:47 +0700

    session complete and table todos complete

--- routes/todos.js
+++ routes/todos.js
@@ -4,9 +4,16 @@ const {isLoggedIn} = require('../helpers/util')
 
 /* GET users listing. */
 module.exports = (db) => {
-router.get('/', isLoggedIn, function(req, res, next) {
-  res.render('todos/list');
-});
+router.get('/', isLoggedIn, async function(req, res, next) {
+    const{rows: todos} = await db.query('SELECT* FROM todos WHERE userid = $1', [req.session.user.id])
+      res.render('todos/list', {todos});
+    });
+    router.post('/add', isLoggedIn, async function(req, res, next) {
+      const {title} = req.body
+      await db.query('INSERT INTO todos (title, userid) VALUES ($1, $2)', [title, req.session.user.id])
+      res.redirect('/todos')
+    });
+
 return router;
 }
 
